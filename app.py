9473143642e82ba8c9543829675deeba2e6b2671
John Ohaeri <Johnxyn@yahoo.com>
2025-12-31 17:20:06 +0100

    added env variables for testing

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, jsonify
+from flask import (Flask, jsonify)
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def hello():
-    return jsonify(
-        status='success',
-        message='Hello, DevOps World!',
-        version='2.0.0'
-    ), 200
+    # It looks for CUSTOM_MESSAGE; if it's not found, it uses a default
+    msg = os.getenv("CUSTOM_MESSAGE", "Default Hello")
+    color = os.getenv("APP_COLOR", "white")
+    return {
+        "message": msg,
+        "color_setting": color,
+        "status": "success",
+        "version": "2.0.0"
+    }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
